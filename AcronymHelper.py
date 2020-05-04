4e2e8fcc28d1ad08583688d82dd712585cf65862
Shanikan <62619626+Shanikan@users.noreply.github.com>
2020-05-03 21:55:35 -0400

    Create AcronymHelper.py
    Printing text one character per line, but seemingly functional otherwise for only TCP.

---
+++ AcronymHelper.py
@@ -0,0 +1,29 @@
+import sys
+import argparse
+import requests
+from bs4 import BeautifulSoup
+import urllib
+
+# Argparse Setup
+
+parser = argparse.ArgumentParser(description="This program exists as a sort of programming flashcard for acronyms and other terminology, by pulling the summary from Wikipedia.")
+parser.add_argument('-p', metavar='date', type=str, nargs='+', help="The requested protocol or acronym to be looked up.")
+args = parser.parse_args()
+try:
+  protocol = str(args.p[0])
+except:
+  print("Use the arugment -h to show the help prompts.")
+  sys.exit()
+
+# Hardcoding certain acronyms that have multiple non-computer related uses to the correct ones
+if protocol == "TCP":
+    protocol = "Transmission_Control_Protocol"
+
+
+url = "https://en.wikipedia.org/wiki/" + protocol
+versions = []
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
+for link in soup.find('p').getText():
+    String = link
+    print(String)
